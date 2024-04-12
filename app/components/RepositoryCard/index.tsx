19b98af24736569a64f700574e86a56945fa842d
David Junior <david.lourenco94@gmail.com>
2024-04-12 16:36:57 -0300

    created PageModules

---
+++ app/components/RepositoryCard/index.tsx
@@ -0,0 +1,12 @@
+import styles from "./styles.module.scss";
+
+export default function RepositoryCard() {
+  return (
+    <div className={styles.container}>
+      <h3>Meu projeto novo</h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, totam?
+      </p>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import styles from "./page.module.scss";
 import UserProfile from "./components/UserProfile";
+import RepositoryCard from "./components/RepositoryCard";
 
 export default function Home() {
   return (
     <main className={styles.main}>
+      <RepositoryCard />
       <UserProfile />
     </main>
   );
